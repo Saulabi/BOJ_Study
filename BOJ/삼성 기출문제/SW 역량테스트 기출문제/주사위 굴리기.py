0ddd50110cffc2443255fac5537a4f34e3148b4d
saulabi <saulabi4523@gmail.com>
2021-07-12 11:16:13 +0900

    change dice

--- BOJ/삼성 기출문제/SW 역량테스트 기출문제/주사위 굴리기.py
+++ BOJ/삼성 기출문제/SW 역량테스트 기출문제/주사위 굴리기.py
@@ -4,12 +4,13 @@ input = sys.stdin.readline
 
 def move(direction):
     if direction == 1:  # 동
-        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
     elif direction == 2:  # 서
-        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
     elif direction == 3:  # 북
-        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
     elif direction == 4:  # 남
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
 
 def dire(direction):
     if direction == 1:
